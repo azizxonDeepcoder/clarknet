ff079ae64ecd548e3ac08179534137abc03e8d94
azizxonDeepcoder <azizxon43211234@gmail.com>
2026-06-14 22:50:14 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ import os
 app = Flask(__name__)
 
 horror_events = [
-    "Tunda yolg‘iz uyda qadam tovushlari eshitildi...",
-    "Oynada ortingda kimdir turgandek tuyuldi...",
-    "Telefon o‘zi qo‘ng‘iroq qildi, lekin ovoz yo‘q edi...",
-    "Qorong‘uda kimdir ismingni pichirladi...",
-    "Eshik ochiq edi, lekin sen yopgan eding..."
+    "Tunda qadam tovushlari eshitildi...",
+    "Oynada kimdir ortingda turgandek...",
+    "Telefon o‘zi qo‘ng‘iroq qildi...",
+    "Qorong‘ida ismni kimdir chaqirdi...",
+    "Eshik ochiq edi, lekin sen yopganding..."
 ]
 
 @app.route("/")
@@ -24,11 +24,7 @@ def enter():
     if age != "18+":
         return "Kirish taqiqlangan ❌"
 
-    return render_template(
-        "home.html",
-        region=region,
-        event=random.choice(horror_events)
-    )
+    return render_template("home.html", region=region, event=random.choice(horror_events))
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
